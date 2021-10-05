186e637fe1ff39325c5152bac6b8a68db0757669
Eugeny Klokov <grafinfector@hotmail.com>
2021-10-06 00:24:46 +0300

    3d projection

--- basic/Draw3D/MainPage.xaml.cs
+++ basic/Draw3D/MainPage.xaml.cs
@@ -8,30 +8,51 @@ namespace Draw3D
 {
     public sealed partial class MainPage : Page
     {
-        Vector4F eye = new Vector4F(0, 200, -400, 0);
+        Vector4F[] box = new Vector4F[8]
+        {
+            new Vector4F(-100,  100, -100, 1),
+            new Vector4F(-100,  100,  100, 1),
+            new Vector4F(-100, -100,  100, 1),
+            new Vector4F(-100, -100, -100, 1),
+            new Vector4F( 100,  100, -100, 1),
+            new Vector4F( 100,  100,  100, 1),
+            new Vector4F( 100, -100,  100, 1),
+            new Vector4F( 100, -100, -100, 1)
+        };
+
+        Vector4F eye = new Vector4F(500, 300, -500, 0);
         Matrix4x4F viewMatrix;
 
         public MainPage()
         {
             this.InitializeComponent();
-            viewMatrix = Matrix4x4F.LookAt(eye, new Vector4F(0, 0, 0, 0), new Vector4F(0, 1, 0, 0));
+            viewMatrix = Matrix4x4F.LookAt(eye, new Vector4F(0, 0, 0, 0), new Vector4F(0, 1, 0, 0));           
         }
 
         // Basic draw cyc
         private void OnDraw(CanvasControl sender, CanvasDrawEventArgs args) 
         { 
             DrawGrid(sender, args.DrawingSession);
+
             var p0 = WorldToScreen(sender, new Vector2F());
+
+            foreach (var p in box)
+            {
+                var pt = Func3D.Mul(viewMatrix, p);
+                pt = Project(pt);
+                var pw = WorldToScreen(sender, new Vector2F(pt.X, pt.Y));
+                args.DrawingSession.FillCircle(pw.X, pw.Y, 2, Colors.Yellow);                
+            }
+
             args.DrawingSession.FillCircle(p0.X, p0.Y, 2, Colors.Red);
+        }
 
-            // Draw old pos
-            p0 = WorldToScreen(sender, new Vector2F(eye.Z, eye.Y));
-            args.DrawingSession.FillCircle(p0.X, p0.Y, 2, Colors.Green);
-
-            var p1 = Vector4F.Zero();
-            p1 = Func3D.Mul(viewMatrix, p1);
-            p0 = WorldToScreen(sender, new Vector2F(p1.Z, p1.Y));
-            args.DrawingSession.FillCircle(p0.X, p0.Y, 2, Colors.Yellow);
+        Vector4F Project(Vector4F v)
+        {
+            var n = 500;
+            var x = n * v.X / v.Z;
+            var y = n * v.Y / v.Z;
+            return new Vector4F(x, y, v.Z, v.W);
         }
 
         private Vector2F WorldToScreen(CanvasControl canvas, Vector2F p)
--- basic/Draw3D/Math3D/Matrix4x4F.cs
+++ basic/Draw3D/Math3D/Matrix4x4F.cs
@@ -1,4 +1,6 @@
-﻿namespace Draw3D.Math3D
+﻿using System;
+
+namespace Draw3D.Math3D
 {
     internal class Matrix4x4F
     {
@@ -20,6 +22,21 @@
         public Matrix4x4F(Vector4F a, Vector4F b, Vector4F c, Vector4F d)
         {
             _data = new Vector4F[4] { a, b, c, d }; 
+        }
+
+        public static Matrix4x4F Perspective(float fov, float aspect, float near, float far)
+        {
+            var a = 1f / MathF.Tan(fov / 2f);
+            var b = a / aspect;
+            var c = (far + near) / (far - near);
+            var d = (-2 * far * near) / (far - near);
+
+            return new Matrix4x4F(
+                new Vector4F(b, 0, 0, 0),
+                new Vector4F(0, a, 0, 0),
+                new Vector4F(0, 0, c, 0),
+                new Vector4F(0, 0, d, 0)
+            );
         }
 
         public static Matrix4x4F LookAt(Vector4F eye, Vector4F target, Vector4F up)
