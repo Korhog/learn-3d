13c607c8c3539d0c23c245dfdc52fc3a959068ac
Eugeny Klokov <grafinfector@hotmail.com>
2021-10-29 00:17:59 +0300

    texture, UV, normal

--- basic/Draw3D/MainPage.xaml.cs
+++ basic/Draw3D/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System;
 using Microsoft.Graphics.Canvas;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Draw3D
 {
@@ -14,6 +15,10 @@ namespace Draw3D
         private float angle = 0.0f;
         private int pixelSize = 1;
         private float pixelOffset = 0.5f;
+
+        private CanvasBitmap cb;
+
+        private readonly Dictionary<(int, int), Color> colorCache = new Dictionary<(int, int), Color>();
 
         Vector4F[] box = new Vector4F[8]
         {
@@ -38,8 +43,76 @@ namespace Draw3D
             new Vertex(new Vector4F( 1,  1, -1, 1)), // 4 A D E
             new Vertex(new Vector4F( 1,  1,  1, 1)), // 5 B D E
             new Vertex(new Vector4F( 1, -1,  1, 1)), // 6 B D F
-            new Vertex(new Vector4F( 1, -1, -1, 1) ) // 7 A D F
+            new Vertex(new Vector4F( 1, -1, -1, 1)) // 7 A D F
         };
+
+        Vertex[] cube = new Vertex[]
+        {
+            // front side
+            new Vertex(new Vector4F(-1,-1,-1, 1), Func3D.Normalyze(new Vector4F(-1,-1,-2, 1)), new Vector2F(0,1)),
+            new Vertex(new Vector4F(-1, 1,-1, 1), Func3D.Normalyze(new Vector4F(-1, 1,-2, 1)), new Vector2F(0,0)),
+            new Vertex(new Vector4F( 1, 1,-1, 1), Func3D.Normalyze(new Vector4F( 1, 1,-2, 1)), new Vector2F(1,0)),
+
+            new Vertex(new Vector4F(-1,-1,-1, 1), Func3D.Normalyze(new Vector4F(-1,-1,-2, 1)), new Vector2F(0,1)),
+            new Vertex(new Vector4F( 1, 1,-1, 1), Func3D.Normalyze(new Vector4F( 1, 1,-2, 1)), new Vector2F(1,0)),
+            new Vertex(new Vector4F( 1,-1,-1, 1), Func3D.Normalyze(new Vector4F( 1,-1,-2, 1)), new Vector2F(1,1)),
+            
+            // back side
+            new Vertex(new Vector4F( 1,-1, 1, 1), Func3D.Normalyze(new Vector4F( 1,-1, 2, 1)), new Vector2F(0,1)),
+            new Vertex(new Vector4F( 1, 1, 1, 1), Func3D.Normalyze(new Vector4F( 1, 1, 2, 1)), new Vector2F(0,0)),
+            new Vertex(new Vector4F(-1, 1, 1, 1), Func3D.Normalyze(new Vector4F(-1, 1, 2, 1)), new Vector2F(1,0)),
+
+            new Vertex(new Vector4F( 1,-1, 1, 1), Func3D.Normalyze(new Vector4F( 1,-1, 2, 1)), new Vector2F(0,1)),
+            new Vertex(new Vector4F(-1, 1, 1, 1), Func3D.Normalyze(new Vector4F(-1, 1, 2, 1)), new Vector2F(1,0)),
+            new Vertex(new Vector4F(-1,-1, 1, 1), Func3D.Normalyze(new Vector4F(-1,-1, 2, 1)), new Vector2F(1,1)),
+            
+            // right side
+            new Vertex(new Vector4F( 1,-1,-1, 1), Func3D.Normalyze(new Vector4F( 2,-1,-1, 1)), new Vector2F(0,1)),
+            new Vertex(new Vector4F( 1, 1,-1, 1), Func3D.Normalyze(new Vector4F( 2, 1,-1, 1)), new Vector2F(0,0)),
+            new Vertex(new Vector4F( 1, 1, 1, 1), Func3D.Normalyze(new Vector4F( 2, 1, 1, 1)), new Vector2F(1,0)),
+
+            new Vertex(new Vector4F( 1,-1,-1, 1), Func3D.Normalyze(new Vector4F( 2,-1,-1, 1)), new Vector2F(0,1)),
+            new Vertex(new Vector4F( 1, 1, 1, 1), Func3D.Normalyze(new Vector4F( 2, 1, 1, 1)), new Vector2F(1,0)),
+            new Vertex(new Vector4F( 1,-1, 1, 1), Func3D.Normalyze(new Vector4F( 2,-1, 1, 1)), new Vector2F(1,1)),
+
+            // left side
+            new Vertex(new Vector4F(-1,-1, 1, 1), Func3D.Normalyze(new Vector4F(-2,-1, 1, 1)), new Vector2F(0,1)),
+            new Vertex(new Vector4F(-1, 1, 1, 1), Func3D.Normalyze(new Vector4F(-2, 1, 1, 1)), new Vector2F(0,0)),
+            new Vertex(new Vector4F(-1, 1,-1, 1), Func3D.Normalyze(new Vector4F(-2, 1,-1, 1)), new Vector2F(1,0)),
+
+            new Vertex(new Vector4F(-1,-1, 1, 1), Func3D.Normalyze(new Vector4F(-2,-1, 1, 1)), new Vector2F(0,1)),
+            new Vertex(new Vector4F(-1, 1,-1, 1), Func3D.Normalyze(new Vector4F(-2, 1,-1, 1)), new Vector2F(1,0)),
+            new Vertex(new Vector4F(-1,-1,-1, 1), Func3D.Normalyze(new Vector4F(-2,-1,-1, 1)), new Vector2F(1,1)),
+
+            // bottom side
+            new Vertex(new Vector4F( 1,-1,-1, 1), Func3D.Normalyze(new Vector4F( 1,-2,-1, 1)), new Vector2F(0,1)),
+            new Vertex(new Vector4F( 1,-1, 1, 1), Func3D.Normalyze(new Vector4F( 1,-2, 1, 1)), new Vector2F(0,0)),
+            new Vertex(new Vector4F(-1,-1, 1, 1), Func3D.Normalyze(new Vector4F(-1,-2, 1, 1)), new Vector2F(1,0)),
+
+            new Vertex(new Vector4F( 1,-1,-1, 1), Func3D.Normalyze(new Vector4F( 1,-2,-1, 1)), new Vector2F(0,1)),
+            new Vertex(new Vector4F(-1,-1, 1, 1), Func3D.Normalyze(new Vector4F(-1,-2, 1, 1)), new Vector2F(1,0)),
+            new Vertex(new Vector4F(-1,-1,-1, 1), Func3D.Normalyze(new Vector4F(-1,-2,-1, 1)), new Vector2F(1,1)),
+
+             // top side
+            new Vertex(new Vector4F(-1, 1,-1, 1), Func3D.Normalyze(new Vector4F(-1, 2,-1, 1)), new Vector2F(0,1)),
+            new Vertex(new Vector4F(-1, 1, 1, 1), Func3D.Normalyze(new Vector4F(-1, 2, 1, 1)), new Vector2F(0,0)),
+            new Vertex(new Vector4F( 1, 1, 1, 1), Func3D.Normalyze(new Vector4F( 1, 2, 1, 1)), new Vector2F(1,0)),
+
+            new Vertex(new Vector4F(-1, 1,-1, 1), Func3D.Normalyze(new Vector4F(-1, 2,-1, 1)), new Vector2F(0,1)),
+            new Vertex(new Vector4F( 1, 1, 1, 1), Func3D.Normalyze(new Vector4F( 1, 2, 1, 1)), new Vector2F(1,0)),
+            new Vertex(new Vector4F( 1, 1,-1, 1), Func3D.Normalyze(new Vector4F( 1, 2,-1, 1)), new Vector2F(1,1))
+        };
+
+        int[] cubeIndicies = new int[] {
+             0, 1, 2, 3, 4, 5,
+             6, 7, 8, 9,10,11,
+            12,13,14,15,16,17,
+            18,19,20,21,22,23,
+            24,25,26,27,28,29,
+            30,31,32,33,34,35
+        };
+
+
 
         int[] indicies = new int[]
         {
@@ -64,25 +137,39 @@ namespace Draw3D
         public MainPage()
         {
             this.InitializeComponent();
+
+            Task.Run(async () =>
+            {
+                var device = CanvasDevice.GetSharedDevice();
+                cb = await CanvasBitmap.LoadAsync(device, "texture.png");      
+            });
+
             viewMatrix = Matrix4x4F.LookAt(eye, new Vector4F(0, 0, 0, 0), new Vector4F(0, 1, 0, 0));
             perspectiveMatrix = Matrix4x4F.Perspective(40, 1, 10);
             worldToScreenMatrix = Matrix4x4F.WorldToScreen(Canvas3D.Size);
             mvp = Matrix4x4F.Multiply(Matrix4x4F.Multiply(viewMatrix, perspectiveMatrix), worldToScreenMatrix);
             Matrix4x4F.Invert(mvp, out mvpInverted);
+
+
         } 
 
         private void OnAnimatedDraw(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
         {
+            if(cb == null)
+            {
+                return;
+            }
+
             var a = angle * MathF.PI / 180.0f;
             rotation = Matrix4x4F.Rotation(a, a, a);
-            //rotation = Matrix4x4F.RotationY(a);
+            //rotation = Matrix4x4F.RotationY(0);
 
             using (var session = args.DrawingSession)
             {
-                DrawIndicies(vertices, indicies, session);
+                DrawIndicies(cube, cubeIndicies, session);
             }
 
-            angle += 0.2f;
+            angle += 1f;
             if (angle > 360.0f)
             {
                 angle -= 360.0f;
@@ -129,12 +216,13 @@ namespace Draw3D
 
             var screenNormal = Func3D.Cross(pbn, pcn);           
             if (screenNormal.Z < 0)
+            //if (true)           
             {
-                var vertices = new List<Vector4F>();
+                var vertices = new List<Vertex>();
 
-                pa = Vector4F.Transform(worldToScreenMatrix, pa);
-                pb = Vector4F.Transform(worldToScreenMatrix, pb);
-                pc = Vector4F.Transform(worldToScreenMatrix, pc);
+                var va = new Vertex(Vector4F.Transform(worldToScreenMatrix, pa), a.Normal, a.UV);
+                var vb = new Vertex(Vector4F.Transform(worldToScreenMatrix, pb), b.Normal, b.UV);
+                var vc = new Vertex(Vector4F.Transform(worldToScreenMatrix, pc), c.Normal, c.UV);
 
                 //var light = Vector4F.Angle(
                 //     Vector4F.Transform(rotation, faceNormal),
@@ -143,42 +231,57 @@ namespace Draw3D
                 //var color = Light(Colors.YellowGreen, light);
 
                 // rasterize
-                var (minY, maxY) = Area(pa, pb, pc);
-                var size = (int)maxY - minY;                
-                
+                var (minY, maxY) = Area(va.Position, vb.Position, vc.Position);
+                var size = (int)maxY - minY;
+
                 var step = (int)minY;
-                while(step < maxY)
+                while (step < maxY)
                 {
                     vertices.Clear();
-                    AddVertexByY(pa, pb, step, vertices);
-                    AddVertexByY(pb, pc, step, vertices);
-                    AddVertexByY(pc, pa, step, vertices);
+                    AddVertexByY(va, vb, step, vertices);
+                    AddVertexByY(vb, vc, step, vertices);
+                    AddVertexByY(vc, va, step, vertices);
 
-                    vertices = vertices.OrderBy(x => x.X).ToList();
+                    vertices = vertices.OrderBy(x => x.Position.X).ToList();
                     if (vertices.Count > 1)
                     {
                         var vertexA = vertices.First();
                         var vertexB = vertices.Last();
 
-                        for (int i = (int)MathF.Floor(vertexA.X); i < (int)MathF.Floor(vertexB.X) + pixelSize; i += pixelSize)
+                        for (int i = (int)MathF.Floor(vertexA.Position.X); i < (int)MathF.Floor(vertexB.Position.X) + pixelSize; i += pixelSize)
                         {
 
                             var point = GetInterpolatedByXY(vertexA, vertexB, i + pixelOffset, step + pixelOffset);
-                            if(point != null)
+                            if (point != null)
                             {
-                                var worldPoint = Vector4F.Transform(mvpInverted, point);
+                                var worldPoint = Vector4F.Transform(mvpInverted, point.Position);
                                 var light = Vector4F.Angle(
+                                     //Vector4F.Transform(rotation, point.Normal),
                                      faceNormal,
                                      Func3D.Normalyze(lightPos - worldPoint));
 
-                                var color = Light(Colors.HotPink, light);
+                                var color = GetFromTexture(point.UV.X, point.UV.Y, cb);
+
+                                //var color = Color.FromArgb(
+                                //    255,
+                                //    (byte)(point.UV.X * 255f),
+                                //    0,
+                                //    (byte)(point.UV.Y * 255f));
+
+                                //var color = Colors.Gray;
+
+                                color = Light(color, light);
                                 session.FillRectangle(i, step, pixelSize, pixelSize, color);
-                            }                            
-                        }                       
+                            }
+                        }
                     }
 
                     step += pixelSize;
                 }
+
+                //DrawLine(a, b, session, Colors.Red);
+                //DrawLine(b, c, session, Colors.Red);
+                //DrawLine(c, a, session, Colors.Red);
             }
         }
 
@@ -213,9 +316,8 @@ namespace Draw3D
             return (pointA, pointB);
         }
 
-        private void AddVertexByY(Vector4F a, Vector4F b, float y, List<Vector4F> list)
+        private void AddVertexByY(Vertex a, Vertex b, float y, List<Vertex> list)
         {
-
             var pointA = GetInterpolatedByY(a, b, y, y + 0.5f);
             if(pointA != null)
             {
@@ -223,13 +325,13 @@ namespace Draw3D
             }
         }
 
-        private Vector4F GetInterpolatedByY(Vector4F a, Vector4F b, float pos, float y)
+        private Vertex GetInterpolatedByY(Vertex a, Vertex b, float pos, float y)
         {
-            if (y > MathF.Max(a.Y, b.Y) || y < MathF.Min(a.Y, b.Y))
+            if (y > MathF.Max(a.Position.Y, b.Position.Y) || y < MathF.Min(a.Position.Y, b.Position.Y))
                 return null;
 
-            var dy = MathF.Abs(a.Y - y);
-            var len = MathF.Abs(a.Y - b.Y);
+            var dy = MathF.Abs(a.Position.Y - y);
+            var len = MathF.Abs(a.Position.Y - b.Position.Y);
 
             if (len == 0)
             {
@@ -238,22 +340,43 @@ namespace Draw3D
 
             var k = dy / len;
 
-            var dx = MathF.Abs(a.X - b.X) * k;
-            var dz = MathF.Abs(a.Z - b.Z) * k;
+            // interpolate position
+            var dx = MathF.Abs(a.Position.X - b.Position.X) * k;
+            var dz = MathF.Abs(a.Position.Z - b.Position.Z) * k;
 
-            var x = a.X > b.X ? a.X - dx : a.X + dx;
-            var z = a.Z > b.Z ? a.Z - dz : a.Z + dz;
+            var x = a.Position.X > b.Position.X ? a.Position.X - dx : a.Position.X + dx;
+            var z = a.Position.Z > b.Position.Z ? a.Position.Z - dz : a.Position.Z + dz;
 
-            return new Vector4F(x, pos, z, 1);
+            var position = new Vector4F(x, pos, z, 1);
+
+            // interpolate normal
+            var dnx = MathF.Abs(a.Normal.X - b.Normal.X) * k;
+            var dny = MathF.Abs(a.Normal.Y - b.Normal.Y) * k;
+            var dnz = MathF.Abs(a.Normal.Z - b.Normal.Z) * k;
+
+            var nx = a.Normal.X > b.Normal.X ? a.Normal.X - dnx : a.Normal.X + dnx;
+            var ny = a.Normal.Y > b.Normal.Y ? a.Normal.Y - dny : a.Normal.Y + dny;
+            var nz = a.Normal.Z > b.Normal.Z ? a.Normal.Z - dnz : a.Normal.Z + dnz;
+
+            var normal = Func3D.Normalyze(new Vector4F(nx, ny, nz, 1));
+
+            // interpolate uv
+            var du = MathF.Abs(a.UV.X - b.UV.X) * k;
+            var dv = MathF.Abs(a.UV.Y - b.UV.Y) * k;
+
+            var u = a.UV.X > b.UV.X ? a.UV.X - du : a.UV.X + du;
+            var v = a.UV.Y > b.UV.Y ? a.UV.Y - dv : a.UV.Y + dv;
+
+            return new Vertex(position, normal, new Vector2F(u, v));
         }
 
-        private Vector4F GetInterpolatedByXY(Vector4F a, Vector4F b, float x, float y)
+        private Vertex GetInterpolatedByXY(Vertex a, Vertex b, float x, float y)
         {
-            if (x > MathF.Max(a.X, b.X) || x < MathF.Min(a.X, b.X))
+            if (x > MathF.Max(a.Position.X, b.Position.X) || x < MathF.Min(a.Position.X, b.Position.X))
                 return null;
 
-            var dx = MathF.Abs(a.X - x);
-            var len = MathF.Abs(a.X - b.X);
+            var dx = MathF.Abs(a.Position.X - x);
+            var len = MathF.Abs(a.Position.X - b.Position.X);
 
             if (len == 0)
             {
@@ -261,9 +384,34 @@ namespace Draw3D
             }
 
             var k = dx / len;
+            var positon = GetInterpolatedPositionByXY(a.Position, b.Position, k, x, y);
+
+
+            // interpolate normal
+            var dnx = MathF.Abs(a.Normal.X - b.Normal.X) * k;
+            var dny = MathF.Abs(a.Normal.Y - b.Normal.Y) * k;
+            var dnz = MathF.Abs(a.Normal.Z - b.Normal.Z) * k;
+
+            var nx = a.Normal.X > b.Normal.X ? a.Normal.X - dnx : a.Normal.X + dnx;
+            var ny = a.Normal.Y > b.Normal.Y ? a.Normal.Y - dny : a.Normal.Y + dny;
+            var nz = a.Normal.Z > b.Normal.Z ? a.Normal.Z - dnz : a.Normal.Z + dnz;
+
+            var normal = Func3D.Normalyze(new Vector4F(nx, ny, nz, 1));
+
+            // interpolate uv
+            var du = MathF.Abs(a.UV.X - b.UV.X) * k;
+            var dv = MathF.Abs(a.UV.Y - b.UV.Y) * k;
+
+            var u = a.UV.X > b.UV.X ? a.UV.X - du : a.UV.X + du;
+            var v = a.UV.Y > b.UV.Y ? a.UV.Y - dv : a.UV.Y + dv;
+
+            return new Vertex(positon, normal, new Vector2F(u,v));
+        }
+
+        private Vector4F GetInterpolatedPositionByXY(Vector4F a, Vector4F b, float k, float x, float y)
+        {
             var dz = MathF.Abs(a.Z - b.Z) * k;
             var z = a.Z > b.Z ? a.Z - dz : a.Z + dz;
-
             return new Vector4F(x, y, z, 1);
         }
 
@@ -317,6 +465,23 @@ namespace Draw3D
 
             mvp = Matrix4x4F.Multiply(Matrix4x4F.Multiply(viewMatrix, perspectiveMatrix), worldToScreenMatrix);
             Matrix4x4F.Invert(mvp, out mvpInverted);
+        }
+
+        private Color GetFromTexture(float u, float v, CanvasBitmap texture)
+        {
+            var tx = (int)((texture.SizeInPixels.Width - 1) * u);
+            
+
+            var ty = (int)((texture.SizeInPixels.Height - 1) * v);
+
+            if(!colorCache.ContainsKey((tx,ty)))
+            {
+                var color = texture.GetPixelColors((int)tx, (int)ty, 1, 1)[0];
+                colorCache[(tx, ty)] = color;
+                return color;
+            }
+
+            return colorCache[(tx, ty)];
         }
     }
 }
--- basic/Draw3D/Math3D/Vertex.cs
+++ basic/Draw3D/Math3D/Vertex.cs
@@ -5,6 +5,17 @@ namespace Draw3D.Math3D
     {
         public Vector4F Position { get; set; }
 
-        public Vertex(Vector4F position) => Position = position;    
+        public Vector4F Normal {  get; set; }
+
+        public Vector2F UV { get; set; }
+
+        public Vertex(Vector4F position) => Position = position;  
+
+        public Vertex(Vector4F position, Vector4F normal, Vector2F uv)
+        {
+            Position = position;
+            Normal = normal;
+            UV = uv; 
+        }
     }
 }
